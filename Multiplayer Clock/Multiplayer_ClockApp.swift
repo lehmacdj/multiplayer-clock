ec302a8835ac49fdd054f7709e8f1514dff45208
Devin Lehmacher <lehmacdj@gmail.com>
2023-07-07 20:11:55 -0400

    Initial Commit

---
+++ Multiplayer Clock/Multiplayer_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplayer_ClockApp.swift
+//  Multiplayer Clock
+//
+//  Created by Devin Lehmacher on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Multiplayer_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
